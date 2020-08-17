d5132d375deb01c2f77a79f883b894bd539c3adf
Xander Bakx <xanderbakx@gmail.com>
2020-08-17 12:41:01 -0400

    installed mapbox and placed first marker

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("console log");
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoieGFuZGVyYmFreCIsImEiOiJja2R5cTViajcxNWV0MnhvYXNkbWFycmpmIn0.IUF7sHd0sheL7g3uOeilzw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: 'mapbox://styles/mapbox/streets-v10',
+});
+
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
